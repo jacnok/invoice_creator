ab929fcc370976a87cb58eb2179bda50d2e881e7
Jefferson C <jcherton@hotmail.com>
2024-01-10 20:51:01 -0600

    Front end is beginning to shape up. Most bugs are cleared, however I'm still missing "payment methods", and I also don't have anything properly set up for edge cases, such as what happens when there's too many charges and discounts.
    Code organization is still non-existent, so refactoring will need to happen in future commits.

--- invoice_input.py
+++ invoice_input.py
@@ -4,6 +4,8 @@ import sys
 
 class InvInput:
     def __init__(self):
+
+
         self.title_list = []
         self.detail_list = []
         self.rationale_list = []
@@ -12,6 +14,24 @@ class InvInput:
         self.percentage_list = []
         self.price_list = []
         self.category_list = []
+
+        self.service = []
+        self.serv_rationale = []
+        self.serv_rate = []
+        self.serv_price = []
+
+        self.disc_type = []
+        self.disc_reason = []
+        self.disc_percs = []
+        self.disc_cost = []
+        self.tax_perc = 0.0
+
+        self.invoice_num = 00000  # cannot be lower than 10000
+        self.client_name = "clientFN1 clientLN2"  # can also just be clientName
+        self.proj_name = "artistName1, songName2 - projectType3"  # can also just be projectName
+        self.delivery_date = "November XX, 2099"  # Month DD, YYYY
+        self.delivery_method = "snail mail via parcel express"  # ex: a YouTube channel upload, a Google Drive upload, etc.
+        self.due_by = "December XX, 2199" # Month DD, YYYY
 
     def get_detail_from_title(self, x):
         return self.detail_list[self.title_list.index(x)]
@@ -48,12 +68,13 @@ class InvInput:
         # borrowing this from https://stackoverflow.com/questions/10993612/how-to-remove-xa0-from-string-in-python
 
         import unicodedata
+        import re
 
         for a in range(len(proj_col_list)):
             for b in range(len(proj_col_list[a])):
                 # print(proj_col_list[a][b])
                 c = unicodedata.normalize("NFKD", str(proj_col_list[a][b]))
-                proj_col_list[a][b] = c
+                proj_col_list[a][b] = re.sub("[’]+", "&rsquo;", c)
 
         # print(proj_col_list)
 
@@ -69,46 +90,44 @@ class InvInput:
         print("\n\nThis is the category list:")
         print(self.category_list)
 
-        invoice_num = self.get_detail_from_title("Invoice Number")
-        print("This is the invoice number: " + invoice_num)
+        self.invoice_num = self.get_detail_from_title("Invoice Number")
+        print("This is the invoice number: " + self.invoice_num)
 
-        client_name = self.get_detail_from_title("Client Name")
-        proj_name = self.get_detail_from_title("Project Name")
-        delivery_date = self.get_detail_from_title("Delivery Date")
-        delivery_method = self.get_detail_from_title("Delivery Method")
-        due_by = self.get_detail_from_title("Due By")
-
-        service = []
-        serv_rationale = []
-        serv_rate = []
-        serv_price = []
-
-        disc_type = []
-        disc_reason = []
-        serv_percs = []
-        disc_cost = []
+        self.client_name = self.get_detail_from_title("Client Name")
+        self.proj_name = self.get_detail_from_title("Project Name")
+        self.delivery_date = self.get_detail_from_title("Delivery Date")
+        self.delivery_method = self.get_detail_from_title("Delivery Method")
+        self.due_by = self.get_detail_from_title("Due By")
 
         for x in range(len(self.category_list)):
             if self.category_list[x] == "Charge":
-                service.append(self.title_list[x])
-                serv_rationale.append(self.rationale_list[x])
-                serv_rate.append(self.rate_list[x])
-                serv_price.append(self.price_list[x])
-                print("Charge was run")
+                self.service.append(self.title_list[x])
+                self.serv_rationale.append(self.rationale_list[x])
+                self.serv_rate.append(self.rate_list[x])
+                self.serv_price.append(self.price_list[x])
+                print("Charge was run")     # debug statement
 
             elif self.category_list[x] == "Discount":
-                disc_type.append(self.title_list[x])
-                disc_reason.append(self.rationale_list[x])
-                serv_percs.append(self.percentage_list[x])
-                disc_cost.append(self.price_list[x])
-                print("Disc was run")
+                self.disc_type.append(self.title_list[x])
+                self.disc_reason.append(self.rationale_list[x])
+                self.disc_percs.append(self.percentage_list[x])
+                self.disc_cost.append(self.price_list[x])
+                print("Disc was run")       # debug statement
 
-        tax_perc = float(self.get_perc_from_category("Tax"))
-        print("This is the tax percentage: " + str((tax_perc * 100)) + "%")
+        print("this is the list of charges:")
+        for x in range(len(self.service)):
+            print(f"\nService: {self.service[x]} "
+                  f"\n\t Service Rationale: {self.serv_rationale[x]}"
+                  f"\n\t Service Rate: {self.serv_rate[x]}"
+                  f"\n\t Service Price: {self.serv_price[x]}"
+                  )
+
+        self.tax_perc = float(self.get_perc_from_category("Tax"))
+        print("This is the tax percentage: " + str((self.tax_perc * 100)) + "%")
 
         p_total = 0.00
 
-        for x in serv_price:
+        for x in self.serv_price:
             p_total = p_total + float(x)
 
         print("This is the subtotal before discounts or taxes: " + "${:.2f}".format(p_total))
@@ -122,53 +141,46 @@ class InvInput:
         #             case 0:
         #                 proj_dict[x].update({})
 
+    def my_gui_creator(self):
+        sGUI.theme('Dark2')
 
+        # gets file that has all the regular data we need for the invoice
+        if len(sys.argv) == 1:
+            event, values = sGUI.Window('Import (Data) Excel File',
+                                        [[sGUI.Text('Document to open')],
+                                         [sGUI.In(), sGUI.FileBrowse()],
+                                         [sGUI.Open(), sGUI.Cancel()]]).read(close=True)
+            f_name1 = values[0]
+        else:
+            f_name1 = sys.argv[1]
 
+        if not f_name1:
+            sGUI.popup_error("Cancelling: no filename supplied.", title="Program Exiting Now.")
+            raise SystemExit("Cancelling: no filename supplied")
+        else:
+            sGUI.popup('The filename you chose was: ', f_name1)
+    #
+    #     # gets file that has all the background data we need for the invoice
+    #     if len(sys.argv) == 1:
+    #         event, values = sGUI.Window('Import (Background) Excel File',
+    #                                     [[sGUI.Text('Document to open')],
+    #                                      [sGUI.In(), sGUI.FileBrowse()],
+    #                                      [sGUI.Open(), sGUI.Cancel()]]).read(close=True)
+    #         f_name2 = values[0]
+    #     else:
+    #         f_name2 = sys.argv[1]
+    #
+    #     if not f_name2:
+    #         sGUI.popup_error("Cancelling: no filename supplied.", title="Program Exiting Now.")
+    #         raise SystemExit("Cancelling: no filename supplied")
+    #     else:
+    #         sGUI.popup('The filename you chose was: ', f_name2)
+    #
+    #     output_place = sGUI.popup_get_folder("Output Folder for HTML:")
 
+        f_name2 = ""        # testing
+        output_place = ""   # testing
 
-def my_gui_creator():
-    sGUI.theme('Dark2')
+        self.importer(f_name1, f_name2, output_place)
 
-    # gets file that has all the regular data we need for the invoice
-    if len(sys.argv) == 1:
-        event, values = sGUI.Window('Import (Data) Excel File',
-                                    [[sGUI.Text('Document to open')],
-                                     [sGUI.In(), sGUI.FileBrowse()],
-                                     [sGUI.Open(), sGUI.Cancel()]]).read(close=True)
-        f_name1 = values[0]
-    else:
-        f_name1 = sys.argv[1]
-
-    if not f_name1:
-        sGUI.popup_error("Cancelling: no filename supplied.", title="Program Exiting Now.")
-        raise SystemExit("Cancelling: no filename supplied")
-    else:
-        sGUI.popup('The filename you chose was: ', f_name1)
-#
-#     # gets file that has all the background data we need for the invoice
-#     if len(sys.argv) == 1:
-#         event, values = sGUI.Window('Import (Background) Excel File',
-#                                     [[sGUI.Text('Document to open')],
-#                                      [sGUI.In(), sGUI.FileBrowse()],
-#                                      [sGUI.Open(), sGUI.Cancel()]]).read(close=True)
-#         f_name2 = values[0]
-#     else:
-#         f_name2 = sys.argv[1]
-#
-#     if not f_name2:
-#         sGUI.popup_error("Cancelling: no filename supplied.", title="Program Exiting Now.")
-#         raise SystemExit("Cancelling: no filename supplied")
-#     else:
-#         sGUI.popup('The filename you chose was: ', f_name2)
-#
-#     output_place = sGUI.popup_get_folder("Output Folder for HTML:")
-
-    f_name2 = ""        # testing
-    output_place = ""   # testing
-
-    x = InvInput()
-
-    x.importer(f_name1, f_name2, output_place)
-
-
-my_gui_creator()
+# my_gui_creator()
--- invoice_output.py
+++ invoice_output.py
@@ -1,4 +1,9 @@
 import invoice_input as io_in
+import jinja2
+import pdfkit
+
+obj = io_in.InvInput()                              # this should be pulling everything from the input files
+obj.my_gui_creator()
 
 # this all needs to go into an Excel sheet that gets called from io_in, and passed along
 my_address_l1 = "XXXX streetFN Avenue"
@@ -6,14 +11,42 @@ my_address_l2 = "Chicago, IL 00000"
 my_phone_num = "XXX-XXX-XXXX"
 
 # this all goes into a different Excel sheet that gets called from io_in, and passed along
-invoice_num = 10000                                 # cannot be lower than 10000
-client_name = "clientFN clientLN"                   # can also just be clientName
-proj_name = "artistName, songName - projectType"    # can also just be projectName
-delivery_date = "January XX, 2099"                  # Month DD, YYYY
-delivery_method = "snail mail"                      # ex: a YouTube channel upload, a Google Drive upload, etc.
+# invoice_num = 10000                                 # cannot be lower than 10000
+# client_name = "clientFN clientLN"                   # can also just be clientName
+# proj_name = "artistName, songName - projectType"    # can also just be projectName
+# delivery_date = "January XX, 2099"                  # Month DD, YYYY
+# delivery_method = "snail mail"                      # ex: a YouTube channel upload, a Google Drive upload, etc.
+
+invoice_num = obj.invoice_num
+client_name = obj.client_name
+proj_name = obj.proj_name
+delivery_date = obj.delivery_date
+delivery_method = obj.delivery_method
+
+money_fmt = "{:.2f}"
+
+
+# def money(x):
+#     return "{:.2f}".format(x)
+
+
+def open_file(x):
+    y = ""
+    readMyFile = open(x, "r")
+    for line in readMyFile:
+        y = y + "\n" + line
+    readMyFile.close()
+    print("File has been closed.")
+    return y
+
+
+# borrowed from this: https://stackoverflow.com/a/38239630 to solve rounding issues
+def round_t(val, digits):
+    return round(val+10**(-len(str(val))-1), digits)
 
 # this will be how we do most of this:
 # https://towardsdatascience.com/how-to-easily-create-a-pdf-file-with-python-in-3-steps-a70faaf5bed5
+
 
 pg1_context = \
     {
@@ -30,10 +63,12 @@ pg1_context = \
 
     }
 
+print(pg1_context)
+
 charge_list = ""                            # will be raw HTML, consisting of <tr> of charge_HTML_forms all smashed together
 disc_list = ""                              # will be raw HTML, consisting of <tr> of disc_HTML_forms all smashed together
 
-due_by = "March XX, 2099"                   # Month DD, YYYY
+due_by = obj.due_by                  # Month DD, YYYY
 
 charge_HTML_form = """
  <tr>
@@ -90,35 +125,80 @@ disc_HTML_form_new = ""
 
 alt_payments = """..."""
 
-service = []
-rationale = []
-rate = []
-price = [0.00]
-p_total = sum(price)
+service = obj.service
+rationale = obj.serv_rationale
+rate = obj.serv_rate
+price = obj.serv_price
 
-disc_type = []
-disc_reason = []
-percentages = []
-disc_cost = []
-tax_perc = 0.00
+p_total = 0.00
+
+for x in price:
+    p_total = round_t((p_total + float(x)), 2)
+
+disc_type = obj.disc_type
+disc_reason = obj.disc_reason
+percentages = obj.disc_percs
+disc_cost = obj.disc_cost
+tax_perc = obj.tax_perc
 
 
 
-for x in service:
+disc_total = 0.00
+
+for x in disc_cost:
+    disc_total = round_t((disc_total + float(x)), 2)
+
+for x in range(len(service)):
     charge_HTML_form_new = charge_HTML_form.format(
                             service=service[x],
                             rationale=rationale[x],
-                            price=price[x])
+                            price=money_fmt.format(float(price[x]))
+    )
     charge_list = charge_list + charge_HTML_form_new
 
+for x in range(len(disc_type)):
+    disc_HTML_form_new = disc_HTML_form.format(
+                            disc_type=disc_type[x],
+                            disc_reason=disc_reason[x],
+                            disc_cost=money_fmt.format(float(disc_cost[x]))
+    )
+    disc_list = disc_list + disc_HTML_form_new
+
+subtotal_full = round_t(p_total - disc_total, 2)
+tax_total = round_t(subtotal_full * tax_perc, 2)
+price_total = round_t(subtotal_full + tax_total, 2)
 
 pg2_context = \
     {
         "charge_list": charge_list,
+        "disc_list": disc_list,
         "due_by": due_by,
-        "price_total": p_total,
-        "tax_percentage": tax_perc,
-        "disc_total": sum(disc_cost),
-        ...: ...
+        "subtotal_init": money_fmt.format(p_total),
+        "subtotal_full": money_fmt.format(subtotal_full),
+        "price_total": money_fmt.format(price_total),
+        "tax_total": money_fmt.format(tax_total),
+        "tax_percentage": (tax_perc * 100),
+        "disc_total": money_fmt.format(disc_total),
+        'invoice_num': invoice_num,
     }
 
+
+def template_creator():
+
+    template_loader = jinja2.FileSystemLoader('./')   # indicates folder where current HTML template is located.
+
+    template_env = jinja2.Environment(loader = template_loader)
+
+    template1 = template_env.get_template("invoice_pg1.html")
+    template2 = template_env.get_template("invoice_pg2.html")
+
+    output_text1 = template1.render(pg1_context)
+    output_text2 = template2.render(pg2_context)
+
+    config = pdfkit.configuration(wkhtmltopdf = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+
+    pdfkit.from_string(output_text1, 'pg1_generated.pdf', configuration=config)
+    pdfkit.from_string(output_text2, 'pg2_generated.pdf', configuration=config)
+
+
+template_creator()
